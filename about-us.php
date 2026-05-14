21ee2d3c18475ee305e9696e367b1761c46bbbcd
Claris <ongzy0406@gmail.com>
2026-05-14 17:34:13 +0800

    wa

--- about-us.php
+++ about-us.php
@@ -7,6 +7,28 @@
     <link rel="stylesheet" href="css/about-us.css">
 </head>
 <body>
+    <div class="banner-wrapper">
+        <header id="header">
+
+            <div class="logo-container">
+                <img src="icon/LogoName.png" class="logo" />
+                <p class="logo-name">Vibe Vacay</p>
+            </div>
+
+            <nav class= "nav-bar">
+                <p class="nav-button active">Home</p>
+                <p class="nav-button">Recommendation</p>
+                <p class="nav-button">Wishlist</p>
+                <p class="nav-button">About Us</p>
+                <p class="nav-button">Log Out</p>
+            </nav>
+
+            <div class="profile-box">
+                <p class="profile">Profile</p>
+                <img src="icon/profile1.jpg" class="profile-icon" alt="Profile"
+                />
+            </div>
+        </header>
     
 </body>
 </html>
