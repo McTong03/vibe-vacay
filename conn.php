7bb66238f56a39d5644400512dba178eb7c0e11c
Wei Ni <wongwni28@gmail.com>
2026-05-18 12:13:08 +0800

    hi

--- conn.php
+++ conn.php
@@ -1,7 +1,3 @@
 <?php
-  $con = mysqli_connect("localhost", "root", "", "vibe-vacay");
-  
-  if (!$con) {
-      die("Connection failed: " . mysqli_connect_error());
-  }
+  $conn = mysqli_connect("localhost", "root", "", "vibe-vacay");
 ?>
